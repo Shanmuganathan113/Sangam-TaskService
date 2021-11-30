8e85481a289eb28db428e9a84a7099ed5325fe6f
Shanmuganathan113 <shan.nathan113@gmail.com>
2021-11-30 12:42:45 +0530

    Committed with latest updates

--- src/main/java/com/sangam/taskservice/web/TaskController.java
+++ src/main/java/com/sangam/taskservice/web/TaskController.java
@@ -24,26 +24,26 @@ public class TaskController {
 	@Autowired
 	TaskService taskService;
 	
-	// URL : http://localhost:8083/task/taskDetails?taskId=task_id&status=status_id&fetch=flag_id
+	// URL : http://localhost:9001/task/taskDetails?taskId=task_id&status=status_id&fetch=flag_id
 	// @RequestMapping(value = Constants.REQUEST_MAPPING_TASK_DETAILS, params = {Constants.REQUEST_PARAMS_TASK_ID,Constants.REQUEST_PARAMS_STATUS,Constants.REQUEST_PARAMS_FLAG}, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	@RequestMapping(value = Constants.REQUEST_MAPPING_TASK_DETAILS, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public Set<TaskDTO> fetchTaskDetails(@RequestParam Map<String,String> listParams) throws Exception{
 		return taskService.fetchTask(listParams) ;
 	}
 	
-	// URL : http://localhost:8083/task/updateTask
+	// URL : http://localhost:9001/task/updateTask
 	@RequestMapping(value = Constants.REQUEST_MAPPING_UPDATE_TASK,method = RequestMethod.POST, consumes = Constants.REQUEST_CONSUME_JSON, produces = MediaType.APPLICATION_JSON_VALUE)
 	public TaskDTO updateTask(@RequestBody TaskDTO taskDTO) throws Exception{
 		return taskService.updateTask(taskDTO);
 	}
 	
-	// URL : http://localhost:8083/task/updateTaskLog
+	// URL : http://localhost:9001/task/updateTaskLog
 	@RequestMapping(value = Constants.REQUEST_MAPPING_UPDATE_TASK_LOG,method = RequestMethod.POST, consumes = Constants.REQUEST_CONSUME_JSON, produces = MediaType.APPLICATION_JSON_VALUE)
 	public LogDTO updateTaskLog(@RequestBody LogDTO logDTO) throws Exception{
 		return taskService.updateTaskLog(logDTO);
 	}
 	
-	// URL : http://localhost:8083/task/filterTask
+	// URL : http://localhost:9001/task/filterTask
 	@RequestMapping(value = Constants.REQUEST_MAPPING_FILTER_TASK, method = RequestMethod.POST,  consumes = Constants.REQUEST_CONSUME_JSON, produces = MediaType.APPLICATION_JSON_VALUE)
 	public Set<TaskDTO> fetchFilterTask(@RequestBody FilterTaskDTO filterTask) throws Exception{
 		return taskService.fetchFilterTask(filterTask) ;
