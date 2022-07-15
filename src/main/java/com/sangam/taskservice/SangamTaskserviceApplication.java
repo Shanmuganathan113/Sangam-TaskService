daacf5cb7edf72e86cc245e43201773b6d97886b
Shanmuganathan113 <shan.nathan113@gmail.com>
2022-07-15 09:23:35 +0530

    Task service test branch

--- src/main/java/com/sangam/taskservice/SangamTaskserviceApplication.java
+++ src/main/java/com/sangam/taskservice/SangamTaskserviceApplication.java
@@ -18,7 +18,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableAspectJAutoProxy
 @EnableSwagger2
 public class SangamTaskserviceApplication implements CommandLineRunner {
-	//t
+	//taskService test branch
 	@Autowired
     private MyConfiguration myConfig;
 
