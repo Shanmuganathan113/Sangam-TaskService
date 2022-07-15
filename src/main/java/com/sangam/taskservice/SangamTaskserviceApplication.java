c73105b3c6861f638d6b6adf00ca5bba7da77c1e
Shanmuganathan113 <shan.nathan113@gmail.com>
2022-07-15 09:14:20 +0530

    added random value

--- src/main/java/com/sangam/taskservice/SangamTaskserviceApplication.java
+++ src/main/java/com/sangam/taskservice/SangamTaskserviceApplication.java
@@ -18,7 +18,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableAspectJAutoProxy
 @EnableSwagger2
 public class SangamTaskserviceApplication implements CommandLineRunner {
-	
+	//1
 	@Autowired
     private MyConfiguration myConfig;
 
